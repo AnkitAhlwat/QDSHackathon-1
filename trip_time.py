d8911a5330111b14dbb9e74cff68ab905dc0c4e7
ankitahlwat <ankitahlwat54@gmail.com>
2023-01-08 05:10:57 -0800

    Add average to account for fuel rate

--- trip_time.py
+++ trip_time.py
@@ -4,30 +4,30 @@ import pandas as pd
 
 trip_data = {}
 
+for file_number in range(10):
+    with open(f'data/data_group{file_number}.json', "r") as file_object:
+        data = json.load(file_object)
 
-with open(f'data/data_group0.json', "r") as file_object:
-    data = json.load(file_object)
-
-df = pd.DataFrame(data)
-
-
-df = df.sort_values(by=['SHOVEL_ID', 'DUMP_ID', 'TRUCK_ID', 'TIMESTAMP'])
+    df = pd.DataFrame(data)
 
 
-for (shovel_id, dump_id), group in df.groupby(["SHOVEL_ID", "DUMP_ID"]):
-    prev_status = None
-    for index, row in group.iterrows():
-        if prev_status == 'Hauling' and row['STATUS'] == 'Dumping':
-            # Calculate the trip time
-            trip_time = row['TIMESTAMP'] - prev_timestamp
+    df = df.sort_values(by=['SHOVEL_ID', 'DUMP_ID', 'TRUCK_ID', 'TIMESTAMP'])
 
-            if (shovel_id, dump_id) in trip_data:
-                trip_data[(shovel_id, dump_id)]['trip_time'] += trip_time
-                trip_data[(shovel_id, dump_id)]['num_trips'] += 1
-            else:
-                trip_data[(shovel_id, dump_id)] = {'trip_time': trip_time, 'num_trips': 1}
-        prev_status = row['STATUS']
-        prev_timestamp = row['TIMESTAMP']
+
+    for (shovel_id, dump_id), group in df.groupby(["SHOVEL_ID", "DUMP_ID"]):
+        prev_status = None
+        for index, row in group.iterrows():
+            if prev_status == 'Hauling' and row['STATUS'] == 'Dumping':
+                # Calculate the trip time
+                trip_time = row['TIMESTAMP'] - prev_timestamp
+
+                if (shovel_id, dump_id) in trip_data:
+                    trip_data[(shovel_id, dump_id)]['trip_time'] += trip_time
+                    trip_data[(shovel_id, dump_id)]['num_trips'] += 1
+                else:
+                    trip_data[(shovel_id, dump_id)] = {'trip_time': trip_time, 'num_trips': 1}
+            prev_status = row['STATUS']
+            prev_timestamp = row['TIMESTAMP']
 
 
 avg_trip_time_data = {}
@@ -39,7 +39,7 @@ for (shovel_id, dump_id), data in trip_data.items():
 avg_trip_time_df = pd.DataFrame.from_dict(avg_trip_time_data, orient='index', columns=['avg_trip_time'])
 
 # Write the avg_trip_time_df DataFrame to a CSV file
-avg_trip_time_df.to_csv('data/truck_id_avg_trip_time_data.csv', index=True)
+avg_trip_time_df.to_csv('data/truck_id_avg_trip_time_data_all.csv', index=True)
 
-print(avg_trip_time_df)
+
 
